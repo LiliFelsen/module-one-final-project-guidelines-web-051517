4631d4afbd228c4cc9dd2ea0c75ffdaf045316be
github username <github email address>
2017-05-31 16:08:56 -0400

    Added User methods, and tested user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,10 +13,32 @@ class User < ActiveRecord::Base
     VisitedPlace.create(money_spent: money_spent, user: self, visits: 1, place: place)
   end
 
+  def total_spending
+    total = 0
+    self.visited_places.each do |visitedplace|
+      total += visitedplace[:money_spent]
+    end
+    total
+  end
+
+  def most_visited_place
+    self.visited_places.max_by(&:visits).place.name
+  end
+
+  def total_spent_at_most_visited_place
+    self.visited_places.max_by(&:visits).money_spent
+  end
+
+  def top_five_most_visited_places
+    self.visited_places.limit(5).sort_by(&:visits).each do |visitedplace|
+      puts visitedplace.place.name
+    end
+  end
+
+  def average_spent_at_most_visited_place
+    total = self.total_spent_at_most_visited_place
+    visits = self.visited_places.max_by(&:visits).visits
+    average = total/visits
+  end
+
 end
-
-
-# def add_visit(place, money_spent)
-#   Places.all.find do |visitedplace|
-#     visitedplace.name == place
-#   end
--- db/migrate/003_create_visited_places.rb
+++ db/migrate/003_create_visited_places.rb
@@ -4,7 +4,7 @@ class CreateVisitedPlaces < ActiveRecord::Migration[4.2]
   def change
     create_table :visited_places do |t|
       t.integer :money_spent
-      t.integer :visits
+      t.integer :visits, :default => 1
       t.references :user
       t.references :place
     end
